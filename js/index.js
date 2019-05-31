bc658fa29223fa93aadc8b472782a27a067b7e3b
Joe Ganobsik <jganobsik@gmail.com>
2019-05-31 20:10:27 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 // your code here
 function getRepositories() {
+  const xhr = new XMLHttpRequest();
   const name = document.getElementById('username').value;
   const uri = 'https://api.github.com' + '/users/' + name + '/repos';
-  const xhr = new XMLHttpRequest();
+  
   xhr.addEventListener('load', displayRepositories);
   xhr.open('GET', uri);
   xhr.send();
