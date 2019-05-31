b3a00b7b556d6b668a618214b6e2b200e0919660
Joe Ganobsik <jganobsik@gmail.com>
2019-05-31 20:12:42 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -9,3 +9,20 @@ function getRepositories() {
   xhr.send();
   return false;
 }
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><h3>' +
+        commit.commit.author.name +
+        ' (' +
+        commit.author.login +
+        ')</h3>' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+}
