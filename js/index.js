ce2cb5ea8d427ca11b76fc8e864a17a3925b9d39
Joe Ganobsik <jganobsik@gmail.com>
2019-05-31 20:17:56 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -44,3 +44,24 @@ function getBranches(el) {
   xhr.open('GET', uri);
   xhr.send();
 }
+
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText);
+  const repoDisplay =
+    '<ul>' +
+    repos
+      .map(repo => {
+        const dataUsername = 'data-username="' + repo.owner.login + '"';
+        const dataRepoName = 'data-repository="' + repo.name + '"';
+        return `
+          <li>
+            <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+          </li>`;
+      })
+      .join('') +
+    '</ul>';
+  document.getElementById('repositories').innerHTML = repoDisplay;
+}
